87ed9df6b5bcdb4dc82eaa2d65579bbf89df170f
Kononovich Elena <Elena.Kononovich@life.com.by>
2018-11-18 22:46:18 +0300

    lesson2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,15 +1,23 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+
+import static java.lang.Thread.sleep;
 
 public class FirstTest {
 
     private AppiumDriver driver;
+
 
     @Before
     public void setUp() throws Exception {
@@ -29,9 +37,77 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
-        System.out.println("First Test run!");
+            waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"), "error_search", 7);
+            waitForElementAndKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java","error_input", 7);
+            waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                    "error_element", 15);
+    }
+
+
+    @Test
+    public void testCancelSearch() {
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"), "err_id_search", 10);
+        waitForElementAndKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java","error_input", 7);
+        waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"), "sdf", 10);
+        waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"), "err_id_close_btn", 10);
+        waitForElementNotPresent(By.id("org.wikipedia:id/search_close_btn"), "search_close_btn is visible", 10);
+    }
+
+
+    @Test
+    public void testCompareArcicleTitle() {
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"), "err_id_search", 10);
+        waitForElementAndKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java","error_input", 7);
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "err_id_search", 10);
+
+        WebElement element_title = waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"), "sdfsfsf", 15);
+        String article_title = element_title.getAttribute("text");
+        Assert.assertEquals("gtrtedref","Java (programming language)",article_title);
 
     }
+
+
+
+
+
+    private WebElement waitForElementPresent(By by, String err_msg, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(err_msg + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+
+
+    private WebElement waitForElementAndClick(By by, String err_msg, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, err_msg, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndKeys(By by, String value, String err_msg, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, err_msg, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+
+
+    private boolean waitForElementNotPresent(By by, String err_msg, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(err_msg + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+
+    private WebElement waitForElementAndClear(By by, String err_msg, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, err_msg, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+
+
 
 
     @After
